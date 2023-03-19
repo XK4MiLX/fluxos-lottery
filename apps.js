c5ea9ad31d75f08a9fc29597aa0b09837861eb6a
XK4MiLX <62837435+XK4MiLX@users.noreply.github.com>
2023-03-19 11:50:27 +0100

    initial

---
+++ apps.js
@@ -0,0 +1,52 @@
+const axios = require('axios');
+const axiosConfig = {
+  timeout: 13456,
+};
+
+function between(min, max) {
+  return Math.floor(
+    Math.random() * (max - min) + min
+  )
+}
+
+
+async function getAppSpecifications() {
+  try {
+    const fluxnodeList = await axios.get('https://api.runonflux.io/apps/globalappsspecifications', axiosConfig);
+    if (fluxnodeList.data.status === 'success') {
+
+      return fluxnodeList.data.data || [];
+    }
+    return [];
+  } catch (e) {
+    return [];
+  }
+}
+
+async function start(type , winners) {
+  const filter = type;
+  console.log('------------------------------------------------------------------------------------------------');
+  console.log(`| Searching on FluxOS for [filter: ${filter}]`);
+  console.log('------------------------------------------------------------------------------------------------');
+  let zelid_array = [];
+  let name_array = [];
+  let winner_list = [];
+  let appsDetails = await getAppSpecifications();
+  for (const appSpecs of appsDetails) {
+   if (filter.some((word) => appSpecs.name.startsWith(word))) {
+       zelid_array.push(`${appSpecs.owner}`);
+       name_array.push(`${appSpecs.name}`);
+   }
+  }
+  for (var i = 0; i < winners; i++){
+    let winnerNumber = between(0,zelid_array.length-1);
+    let index = winner_list.indexOf(zelid_array[winnerNumber]);
+    if (index == -1) {
+      winner_list.push(zelid_array[winnerNumber]);
+      console.log(`| Winner ${i+1} =>  ${zelid_array[winnerNumber]}, ${name_array[winnerNumber]}`);
+    } else { i = i-1 }
+  }
+  console.log(`-------------------------------------------------------------------------------------[FOUND: ${zelid_array.length}]`);
+}
+
+start(process.argv.slice(3),process.argv[2]);
